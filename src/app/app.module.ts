812de7a1b97fe3b3b3e25dcdfae82790bf94cbbb
wajdi gabsi <gabsiwajdi2014@gmail.com>
2024-03-27 23:33:29 +0100

    navbar et acceuil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    NavbarComponent,
+    WhatWeDoComponent
   ],
   imports: [
     BrowserModule,
